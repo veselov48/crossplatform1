033fde67c2ba17504bbbb2a10ef6f413859e4839
veselov48 <veselov.as1704@asugubkin.ru>
2020-04-23 15:38:44 +0300

    project tempalate

---
+++ veselov/veselov/models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace veselov.Models
+{
+    public class Company
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }       
+        public ICollection<Worker> Workers { get; set; }
+
+        public int WorkersCount()
+        {
+            return Workers.Count;
+        }
+
+    }
+}
