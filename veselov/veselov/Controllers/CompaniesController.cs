be8b8c179d992f5e1af3881a28a9e014bee23cc6
veselov48 <63636747+veselov48@users.noreply.github.com>
2020-04-23 15:07:27 +0300

    project template

---
+++ veselov/veselov/Controllers/CompaniesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using veselov.Models;
+
+namespace veselov.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly CompanyContext _context;
+
+        public CompaniesController(CompanyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Companies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Company>>> GetCompanies()
+        {
+            return await _context.Companies.ToListAsync();
+        }
+
+        // GET: api/Companies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Company>> GetCompany(long id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return company;
+        }
+
+        // PUT: api/Companies/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompany(long id, Company company)
+        {
+            if (id != company.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(company).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Companies
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Company>> PostCompany(Company company)
+        {
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCompany", new { id = company.Id }, company);
+        }
+
+        // DELETE: api/Companies/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Company>> DeleteCompany(long id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+
+            return company;
+        }
+
+        private bool CompanyExists(long id)
+        {
+            return _context.Companies.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ veselov/veselov/Controllers/WorkersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using veselov.Models;
+
+namespace veselov.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkersController : ControllerBase
+    {
+        private readonly WorkerContext _context;
+
+        public WorkersController(WorkerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Workers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Worker>>> GetWorkers()
+        {
+            return await _context.Workers.ToListAsync();
+        }
+
+        // GET: api/Workers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Worker>> GetWorker(long id)
+        {
+            var worker = await _context.Workers.FindAsync(id);
+
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+            return worker;
+        }
+
+        // PUT: api/Workers/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWorker(long id, Worker worker)
+        {
+            if (id != worker.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(worker).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WorkerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Workers
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Worker>> PostWorker(Worker worker)
+        {
+            _context.Workers.Add(worker);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWorker", new { id = worker.Id }, worker);
+        }
+
+        // DELETE: api/Workers/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Worker>> DeleteWorker(long id)
+        {
+            var worker = await _context.Workers.FindAsync(id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+            _context.Workers.Remove(worker);
+            await _context.SaveChangesAsync();
+
+            return worker;
+        }
+
+        private bool WorkerExists(long id)
+        {
+            return _context.Workers.Any(e => e.Id == id);
+        }
+    }
+}
